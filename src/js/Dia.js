10c7f0f063715bc7282d24e6fa257a696b8f3357
Fábio Veiga <veigabfabio@hotmail.com>
2023-03-27 11:56:46 -0200

    Comentários e lógica

--- src/js/Dia.js
+++ src/js/Dia.js
@@ -15,7 +15,7 @@ class Dia {
         this.titulo = `${dia} de ${mes}, ${ano}`
     }
 
-    static buscar(){
+    static buscar() {
         // TODO | Busca no JSON se esse dia tem algum registro.
     }
 
@@ -26,21 +26,28 @@ class Dia {
 
     static abrir() {
         this.formatar();
+        // this.buscar();
+        // TODO: this.buscar verifica se existem registros no dia e só então chama this.inserir
         this.inserir();
 
+        // Mostra a janela do dia selecionado e desfoca o conteúdo atrás.
         $(".janela-dia")[0].style.display = 'initial'
-        $(".janela-inicio")[0].classList.add("blur")
         $(".outside")[0].style.display = "initial";
+        $(".janela-inicio")[0].classList.add("blur")
 
-        function close(){
-            $(".outside")[0].style.display = "none";
-            $(".janela-dia")[0].style.display = 'none'
-            $(".janela-inicio")[0].classList.remove("blur")
-            $(".outside")[0].removeEventListener("click", close)    
-            $(".fechar-dia")[0].removeEventListener("click", close)        
-        }
-        $(".outside")[0].addEventListener("click", close)
-        $(".fechar-dia")[0].addEventListener("click", close)
+        // Quando o usuário clica no símbolo de voltar, ou fora da janela do dia, fecha a janela.
+        $(".outside")[0].addEventListener("click", this.fechar)
+        $(".fechar-dia")[0].addEventListener("click", this.fechar)
 
+    }
+
+    static fechar() {
+        // Esconde a janela aberta e tira o desfoque do resto do conteúdo
+        $(".outside")[0].style.display = "none";
+        $(".janela-dia")[0].style.display = 'none'
+        $(".janela-inicio")[0].classList.remove("blur")
+        // Remove listeners de click, já que serão adicionados novamente na próxima execução.
+        $(".outside")[0].removeEventListener("click", close)
+        $(".fechar-dia")[0].removeEventListener("click", close)
     }
 }
